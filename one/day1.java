f1507284c912c610bfebffb54c9a863af42a8b9e
Minskoleopc <minskoleopc@gmail.com>
2023-09-08 07:55:43 +0530

    added

--- one/day1.java
+++ one/day1.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class day1 {
+public class day1{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
---
+++ one/day7.java
@@ -0,0 +1,12 @@
+package one;
+
+public class day7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// waits - Pragati
+
+	}
+
+}
