8de1ab963f1d0332c1ca7327f79884e4c81c05aa
Minskoleopc <minskoleopc@gmail.com>
2023-09-09 08:03:13 +0530

    added

--- one/class7b.java
+++ one/class7b.java
@@ -5,25 +5,32 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class day7 {
+public class class7b {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
 		
 		System.setProperty("webdriver.chrome.driver",
 				"C:\\Users\\ankurjain\\Desktop\\chromedriver\\chromedriver\\chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://www.saucedemo.com/");
 		//driver.findElement(By.id("user-n"));
 		driver.findElement(By.id("user-name")).sendKeys("standard_user");
 		driver.findElement(By.name("password")).sendKeys("secret_sauce");
 		driver.findElement(By.className("submit-button")).click();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.findElement(By.cssSelector(".bm-burger-button")).click();
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		driver.findElement(By.cssSelector("#logout_sidebar_link")).click();
 		driver.close();
-
+		
+		
+		
 	}
 
 }
